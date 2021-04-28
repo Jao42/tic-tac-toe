6c1a32e82a420800d62fdcfed7876aa0c841c89f
Jao42 <joaovictormedeiros41@gmail.com>
2021-04-28 09:18:59 -0300

    refactoring some functions

--- script.js
+++ script.js
@@ -92,6 +92,29 @@ function playerChoice(e, i, arrayX, arrayY, arrayBoard, activeX, player1, player
   return result;
 }
 
+function resetDefault(result) {
+       
+  displayResult(result);
+  arrayBoard = Array(9).fill();
+  arrayX = [];
+  arrayY = [];
+  cleanScreen(gameBoard.quadrants);
+  activeX = true; activeY = false;
+  return 0;
+}
+
+function changeCurrentPlayer() {
+
+  activeX = !activeX;
+  activeY = !activeY;
+}
+
+function randint(rangeLen) {
+  return Math.floor(Math.random() * rangeLen);
+
+}
+
+
 modeButtons.forEach((e) => {
   e.addEventListener('click', () => {
   initialScreen.classList.add('modal-close');
@@ -100,49 +123,45 @@ modeButtons.forEach((e) => {
   })
 })
 
+
 const gameBoard = {
   quadrants: document.querySelectorAll('.game-table > div')
 
 }
 
 let arrayBoard = Array(9).fill();
+
 let arrayX = [];
 let arrayY = [];
 
 let activeX = true;
 let activeY = false;
-player1 = player('player1', '⨯', false)
-player2 = player('player2', '◯', false)
+
+const player1 = player('player1', '⨯', false)
+const player2 = player('player2', '◯', false)
 
 let result = 0;
 
 gameBoard.quadrants.forEach((e, i) => {
   e.addEventListener('click', () => {
     if (!e.textContent) {
+
       result = playerChoice(e, i, arrayX, arrayY, arrayBoard, activeX, player1, player2);
-      activeX = !activeX;
-      activeY = !activeY;
 
-      
-      if (result) {
-      displayResult(result);
-      arrayBoard = Array(9).fill();
-      arrayX = [];
-      arrayY = [];
-      cleanScreen(gameBoard.quadrants);
-      
-      }
+      if (result) resetDefault(result);
+      else changeCurrentPlayer();
 
-      else if ((!result) && (isMachine)) {
-        do {indexRandom = Math.floor(Math.random() * 9);} while (arrayY.includes(indexRandom) || arrayX.includes(indexRandom)); 
+      if ((!result) && (isMachine)) {
+        do {indexRandom = randint(9)} while (arrayY.includes(indexRandom) || arrayX.includes(indexRandom)); 
+
         quadrantRandom = gameBoard.quadrants[indexRandom];
         sleep(300);
-        playerChoice(quadrantRandom, indexRandom, arrayX, arrayY, arrayBoard, activeX, player1, player2);
-        activeX = !activeX;
-        activeY = !activeY;
+        result = playerChoice(quadrantRandom, indexRandom, arrayX, arrayY, arrayBoard, activeX, player1, player2);
 
+        if (result) resetDefault(result);
+        else changeCurrentPlayer();
       }
-    }
+  }
 
   })
 })
